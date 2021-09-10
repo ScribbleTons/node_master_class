b17476500b7f2e3b6a04bc4c3534b7cc0e03c53f
ScribbleTons <ugwuokeemmanuel125@gmail.com>
2021-09-10 23:07:33 +0100

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 //Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 //The server should respond to all request with a string
 const server = http.createServer(function (req, res) {
@@ -15,10 +16,38 @@ const server = http.createServer(function (req, res) {
   //Get the path
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, "");
-  //Send the response
-  res.end("Hello world\n");
-  //Log the part on terminal
-  console.log("Request received on pathname: " + trimmedPath);
+
+  //Get query string parameters
+  const queryStringObject = parsedUrl.query;
+
+  //Get HTTP method
+  const method = req.method.toLowerCase();
+
+  //Get headers as object
+  const header = req.headers;
+
+  //Get a payload if any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+
+  req.on("data", function (data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", function () {
+    buffer += decoder.end();
+
+    //Send the response
+    res.end("Hello world\n");
+
+    //Log the part on terminal
+    console.group("Dev logs");
+    console.log(`Request received on pathname: ${trimmedPath}`);
+    console.log("query paramenters", queryStringObject);
+    console.log("header", header);
+    console.log("Body: " + buffer);
+    console.groupEnd();
+  });
 });
 
 //start the server and have it listen to a port
